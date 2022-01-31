c6fad065b057e290d158f92da555b92f5ec6bf1e
ReidMcLain <reidmm@gmail.com>
2022-01-31 15:26:19 -0600

    Cleaned up some stuff

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
-let fullName = 'Reid Michael McLain'
+const fullName = 'reid michael mclain';
+const career = 'Web Dev Student'
+const description = 'the cake is a lie';
+
 let interests = ['Chess', 'Auburn sports', 'Dungeons and Dragons']
-let jobTitle = 'Repair technician';
-let displayPosition = [
+
+const pastPositions = [
   {
     Company: 'uBreakIfix',
     Title: 'Repair technician',
-    Description: 'Description'
+    Description: 'Repaired iPhones and Samsungs with all varieties of technology'
   },
   {
     Company: 'Zaxbys',
     Title: 'Cashier/cook',
     Description: 'I took money for orders, then eventually cooked food'
-  }
+  },
+  {
+    Company: 'Innovate Birmingham',
+  Title: 'Student',
+  Description: 'Full Stacks and professional development'
+}
 ]
-let skills = ['Some skills here']
 
-console.log('name: ' + fullName.toUpperCase())
-console.log('Career: Full Stack Student')
-console.log('Description: The cake is a lie')
-console.log(' ')
+function displaySkills(skill, cool) {
+  if (cool) {
+    console.log('* BAM: ' + skill)
+  } else {
+    console.log('* ' + skill)
+  }
+}
+
+// function displayPosition(Company, Title, Description){
+//   console.log(Company + ' at ' + Title + ' - ' + Description)
+// }
+console.log('Name: ' + fullName.toUpperCase());
+console.log('Career: ' + career);
+console.log('Descriptions: ' + description);
+console.log(' ');
 console.log('My Interests:')
 
-displayPastPosition('mcdonalds','cashier', 'the cake is a lie')
-
 for (let i = 0; i < interests.length; i++) {
-  console.log('* ' + interests[i])
+  console.log("* " + interests[i])
 }
 
 console.log(' ')
+
 console.log('My Previous Experience:')
 
-function displayPastPosition(companyName,title,Description){
-  console.log(companyName + 'Zaxbys' + ' ' + Description + ' ')
+for (let i = 0; i < pastPositions.length; i++) {
+  console.log(pastPositions[i].Title + ' at ' + pastPositions[i].Company + ' ' + pastPositions[i].Description)
 }
-// let pastPositions = (skill, cool) => {
-//     if (cool) {
-//       console.log('*' BAM: ${skill});
-//     } else {
-//       console.log('*' ${skill});
-//     }
-//   };
+
+// displayPosition('uBreakIfx', 'Repair Technician', 'Repaired iPhones and Samsungs with all varieties of technology');
+// displayPosition('Innovate Bham', 'Student', 'Full Stacks and professional development' );
+// displayPosition('Zaxbys', 'Cashier/Cook', 'I took money for orders, then eventually cooked food');
+
+console.log(' ');
+console.log('My Skills: ');
+
+displaySkills('solving rubix cube', true);
+displaySkills('being an axe murderer', false);
+displaySkills('Open mindedness', true);
+displaySkills('being late', false);
